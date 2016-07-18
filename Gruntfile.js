ec24b0c4c6d5ff463c5d675a1289e8fa77bb1d9d
Rafael Pedrola <rafinskipg@gmail.com>
2016-07-18 16:09:47 +0200

    Stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -336,7 +336,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'fonts/{,*/}*.*'
           ]
         }, {
           expand: true,
